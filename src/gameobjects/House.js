7ba460ec4011bd9555275dbcfa6df46049b44c62
recardona <me@rogel.io>
2013-02-28 13:11:59 -0500

    Updated House.js to use the new art assets for the game.

--- src/gameobjects/House.js
+++ src/gameobjects/House.js
@@ -3,31 +3,32 @@ function House(properties) {
     
     this.sprite;
     
-    //Building
+    //Top-left house
     if(properties.type == 1) {
         this.sprite = new jaws.Sprite({image:"./assets/art/house1_topleft.png", anchor:"center", x:properties.x, y:properties.y});
     }
     
-    //Cabin
+    //Top-right house
     else if(properties.type == 2) {
-        this.sprite = new jaws.Sprite({image:"./assets/art/cabin_1.png", anchor:"center", x:properties.x, y:properties.y});
+        this.sprite = new jaws.Sprite({image:"./assets/art/house2_topright.png", anchor:"center", x:properties.x, y:properties.y});
     }
     
-    //Hut
+    //Bottom-left house
     else if(properties.type == 3) {
-        this.sprite = new jaws.Sprite({image:"./assets/art/hut_1.png", anchor:"center", x:properties.x, y:properties.y});
+        this.sprite = new jaws.Sprite({image:"./assets/art/house3_bottomleft.png", anchor:"center", x:properties.x, y:properties.y});
     }
     
-    //Shop
+    //Bottom-middle house
     else if(properties.type == 4) {
-        this.sprite = new jaws.Sprite({image:"./assets/art/shop_1.png", anchor:"center", x:properties.x, y:properties.y});
+        this.sprite = new jaws.Sprite({image:"./assets/art/house4_bottommiddle.png", anchor:"center", x:properties.x, y:properties.y});
     }
     
-    //Market
+    //Bottom-right house
     else if(properties.type == 5) {
-        this.sprite = new jaws.Sprite({image:"./assets/art/market_blueRed.png", anchor:"center", x:properties.x, y:properties.y});
+        this.sprite = new jaws.Sprite({image:"./assets/art/house5_bottomright.png", anchor:"center", x:properties.x, y:properties.y});
     }
     
+    //Boundary
     else if(properties.type == 6) {
         this.sprite = new jaws.Sprite({image:"./assets/art/gate.png", anchor:"center", x:properties.x, y:properties.y});
     }
