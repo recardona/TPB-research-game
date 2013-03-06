a4e77bed9a670fb22c8eea6298a18235260bcb95
Rogelio E. Cardona-Rivera <me@rogel.io>
2013-03-05 23:36:53 -0500

    [#7] Initial version of the Title Screen with it's corresponding art.

--- src/states/MenuState.js
+++ src/states/MenuState.js
@@ -6,25 +6,46 @@
  *
  */
 function MenuState() {
-  var index = 0
-  var items = ["Start", "About"]
+  var index = 0;
+  var items = ["Start", "About"];
+  var title_anim;
+  var title_img;
+  var lightbulb_on = true;
+  
 
   this.setup = function() {
-    index = 0
-    jaws.on_keydown(["down","s"],       function()  { index++; if(index >= items.length) {index=items.length-1} } )
-    jaws.on_keydown(["up","w"],         function()  { index--; if(index < 0) {index=0} } )
-    jaws.on_keydown(["enter","space"],  function()  { if(items[index]=="Start") {jaws.switchGameState(PlayState, {fps: 41 }) } } )
+    index = 0;
+    jaws.on_keydown(["down","s"],       function()  { index++; if(index >= items.length) {index=items.length-1} } );
+    jaws.on_keydown(["up","w"],         function()  { index--; if(index < 0) {index=0} } );
+    jaws.on_keydown(["enter","space"],  function()  { if(items[index]=="Start") {jaws.switchGameState(PlayState, {fps: 41 }) } } );
+    title_anim = new jaws.Animation({sprite_sheet: "./assets/art/title_animation.png", frame_size:[874,579],loop:true});
+    title_img  = new jaws.Sprite({x:jaws.width/2, y:jaws.height/2, anchor:"center"});
+    title_img.setImage(title_anim.next());
+  }
+  
+  this.update = function() {
+  	if(lightbulb_on) {
+		var rand = random_sigmoid();
+		
+  		if(rand > 0.7 || rand < -0.7) {
+  			rand = Math.abs(rand);
+  			lightbulb_on = false;
+  			title_img.setImage(title_anim.next());
+  			setTimeout(function(){lightbulb_on = true;},750*rand);
+  		}  		
+  	} 
   }
 
   this.draw = function() {
-    jaws.context.clearRect(0,0,jaws.width,jaws.height)
+    jaws.context.clearRect(0,0,jaws.width,jaws.height);
+    title_img.draw();
     for(var i=0; items[i]; i++) {
       
-      jaws.context.font = "bold 120pt terminal";
-      jaws.context.lineWidth = 20
-      jaws.context.fillStyle =  (i == index) ? "Red" : "Black"
-      jaws.context.strokeStyle =  "rgba(200,200,200,0.0)"
-      jaws.context.fillText(items[i], 30, jaws.height/2.5 + i * (200))
+      jaws.context.font = "36pt Denk One";
+      jaws.context.lineWidth = 20;
+      jaws.context.fillStyle =  (i == index) ? "#E5F361" : "#62625F";
+      jaws.context.strokeStyle =  "rgba(200,200,200,0.0)";
+      jaws.context.fillText(items[i], jaws.width*3/4, jaws.height*7/8 + i * (50));
     }  
   }
 }
